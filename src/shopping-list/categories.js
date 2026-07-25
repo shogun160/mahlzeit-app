547a631e564ee936eac7b8e16819c843fd521d60
shogun160 <oliver.wosnitza@gmail.com>
2026-07-25 18:43:39 +0200

    feat(shopping-list): category order + german labels

---
+++ src/shopping-list/categories.js
@@ -0,0 +1,12 @@
+// Kategorie-Reihenfolge in der Einkaufsliste (typisches Supermarkt-Layout:
+// Frisch außen, Trocken/Gewürze innen, Öl & Sonstiges am Ende).
+export const CAT_ORDER = ['frisch', 'trocken', 'gewuerze', 'oel', 'sonstig'];
+
+// Deutsche Anzeige-Labels. Bewusst kurz gehalten (Rebuild-UI ist knapper als main).
+export const CAT_LABELS = {
+  frisch: 'Frische',
+  trocken: 'Trocken',
+  gewuerze: 'Gewürze',
+  oel: 'Öl / Fett',
+  sonstig: 'Sonstiges',
+};
